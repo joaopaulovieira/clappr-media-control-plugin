7b1a57dcf862851e2a806f0b9ba9add5b2c182ab
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-17 20:02:42 -0300

    refactor(public): disable Clappr reset styles

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -4,6 +4,7 @@ const player = new Clappr.Player({
   source: 'http://clappr.io/highline.mp4',
   poster: 'http://clappr.io/poster.png',
   playback: { controls: false },
+  includeResetStyle: false,
 })
 
 player.attachTo(playerElement)
