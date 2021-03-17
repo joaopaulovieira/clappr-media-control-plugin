789ec7cef1924a628c87fc8af434db036b64f580
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-17 20:02:00 -0300

    refactor(public): prefer media control plugin over default video element controls

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -3,8 +3,7 @@ const playerElement = document.getElementById('player-wrapper')
 const player = new Clappr.Player({
   source: 'http://clappr.io/highline.mp4',
   poster: 'http://clappr.io/poster.png',
-  playback: { controls: true },
-  plugins: [window.MediaControlPlugin],
+  playback: { controls: false },
 })
 
 player.attachTo(playerElement)
