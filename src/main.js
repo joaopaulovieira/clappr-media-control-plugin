d76fc7fc7d4d5302c7ba4327a791d9d4af6c25ea
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-17 21:38:43 -0300

    feat(main): export MediaControlComponentPlugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import MediaControlPlugin from './base/media_control/media_control'
+import MediaControlComponentPlugin from './base/media_control_component/media_control_component'
 export {
   MediaControlPlugin as MainPlugin,
+  MediaControlComponentPlugin as BaseComponentPlugin,
 }
