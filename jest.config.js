41a027c117ce8429d55edc085ba9157c21f69d36
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-17 21:42:58 -0300

    test(jest): config mock for svg files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.html$': '<rootDir>/src/__mocks__/htmlMock.js',
+    '^.+\\.svg$': '<rootDir>/src/__mocks__/svgMock.js',
   },
   moduleNameMapper: { '\\.(scss)$': '<rootDir>/src/__mocks__/styleMock.js' },
   collectCoverageFrom: ['src/*/*.js', 'src/*/*/*.js'],
