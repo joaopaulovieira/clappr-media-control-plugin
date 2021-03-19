414ac66f2aefe7c018199f5b7dd2959b16ae20e6
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-19 01:12:21 -0300

    feat(main): export PlayPauseButtonPlugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import MediaControlPlugin from './base/media_control/media_control'
 import MediaControlComponentPlugin from './base/media_control_component/media_control_component'
+import PlayPauseButtonPlugin from './components/play_pause_button/play_pause_button'
 export {
   MediaControlPlugin as MainPlugin,
   MediaControlComponentPlugin as BaseComponentPlugin,
+  PlayPauseButtonPlugin,
 }
