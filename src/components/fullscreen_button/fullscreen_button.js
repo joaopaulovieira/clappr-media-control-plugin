85a1ab2d1f7cee1231a7398ebbf6db49bf823c85
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-19 18:22:02 -0300

    feat(components): create FullscreenButtonPlugin

---
+++ src/components/fullscreen_button/fullscreen_button.js
@@ -0,0 +1,35 @@
+import { Events, Styler, Utils } from '@clappr/core'
+import MediaControlComponentPlugin from '../../base/media_control_component/media_control_component'
+
+export default class FullscreenButtonPlugin extends MediaControlComponentPlugin {
+  get name() { return 'fullscreen_button' }
+
+  bindEvents() {
+    const coreEventListenerData = [{ object: this.core, event: Events.CORE_ACTIVE_CONTAINER_CHANGED, callback: this.onContainerChanged }]
+    coreEventListenerData.forEach(item => this.stopListening(item.object, item.event, item.callback))
+    coreEventListenerData.forEach(item => this.listenTo(item.object, item.event, item.callback))
+  }
+
+  onContainerChanged() {
+    this.container && this.stopListening(this.container)
+    this.container = this.core.activeContainer
+    if (!this.container) return
+    this.bindContainerEvents()
+  }
+
+  bindContainerEvents() {
+  }
+
+  toggle() {
+  }
+
+  changeIcon() {
+  }
+
+  render() {
+    if (this.isRendered || !Utils.Fullscreen.fullscreenEnabled()) return
+    this.$el[0].innerHTML = ''
+    this.isRendered = true
+    return this
+  }
+}
---
+++ src/components/fullscreen_button/fullscreen_button.test.js
@@ -0,0 +1,83 @@
+import { Events, Core, Container, Playback, Utils } from '@clappr/core'
+import FullscreenButtonPlugin from './fullscreen_button'
+import MediaControlComponentPlugin from '../../base/media_control_component/media_control_component'
+
+const setupTest = (options = {}, fullSetup = false) => {
+  const core = new Core(options)
+  const plugin = new FullscreenButtonPlugin(core)
+  core.addPlugin(plugin)
+
+  const response = { core, plugin }
+  fullSetup && (response.container = new Container({ playerId: 1, playback: new Playback({}) }))
+
+  return response
+}
+
+describe('FullscreenButtonPlugin', function() {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    const response = setupTest({}, true)
+    this.core = response.core
+    this.container = response.container
+    this.core.activeContainer = this.container
+    this.plugin = response.plugin
+  })
+
+  test('is loaded on core plugins array', () => {
+    expect(this.core.getPlugin(this.plugin.name).name).toEqual('fullscreen_button')
+  })
+
+  describe('bindEvents method', () => {
+    test('stops the current listeners before add new ones', () => {
+      jest.spyOn(this.plugin, 'stopListening')
+      this.plugin.bindEvents()
+
+      expect(this.plugin.stopListening).toHaveBeenCalled()
+    })
+
+    test('register onContainerChanged method as callback for CORE_ACTIVE_CONTAINER_CHANGED event', () => {
+      jest.spyOn(FullscreenButtonPlugin.prototype, 'onContainerChanged')
+      const { core, plugin } = setupTest()
+      core.trigger(Events.CORE_ACTIVE_CONTAINER_CHANGED)
+
+      expect(plugin.onContainerChanged).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('onContainerChanged method', () => {
+    test('removes all listeners from old container reference', () => {
+      jest.spyOn(this.plugin, 'stopListening')
+      this.plugin.onContainerChanged()
+
+      expect(this.plugin.stopListening).toHaveBeenCalledWith(this.container)
+    })
+
+    test('saves core.activeContainer reference locally', () => {
+      this.plugin.onContainerChanged()
+
+      expect(this.plugin.container).toEqual(this.core.activeContainer)
+    })
+
+    test('calls bindContainerEvents method', () => {
+      jest.spyOn(this.plugin, 'bindContainerEvents')
+      this.plugin.onContainerChanged()
+
+      expect(this.plugin.bindContainerEvents).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('render method', () => {
+    beforeEach(() => {
+      jest.spyOn(Utils.Fullscreen, 'fullscreenEnabled').mockImplementation(() => true)
+      jest.spyOn(this.plugin, 'render')
+      jest.spyOn(this.plugin, 'changeIcon')
+    })
+
+    test('sets isRendered flag to true', () => {
+      this.plugin.isRendered = false
+      this.plugin.render()
+
+      expect(this.plugin.isRendered).toBeTruthy()
+    })
+  })
+})
