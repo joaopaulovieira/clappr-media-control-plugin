a57c7db3cd70378ec4c1de75fd22c59d8c91e39e
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-19 18:56:51 -0300

    feat(main): export FullscreenButtonPlugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import MediaControlPlugin from './base/media_control/media_control'
 import MediaControlComponentPlugin from './base/media_control_component/media_control_component'
 import PlayPauseButtonPlugin from './components/play_pause_button/play_pause_button'
+import FullscreenButtonPlugin from './components/fullscreen_button/fullscreen_button'
 export {
   MediaControlPlugin as MainPlugin,
   MediaControlComponentPlugin as BaseComponentPlugin,
   PlayPauseButtonPlugin,
+  FullscreenButtonPlugin,
 }
