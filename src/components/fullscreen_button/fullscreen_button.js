11287a162179e2e1f7cf5a468e112b618de46919
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-19 18:23:45 -0300

    refactor(fullscreen_button): define plugin render on first media control layer

--- src/components/fullscreen_button/fullscreen_button.js
+++ src/components/fullscreen_button/fullscreen_button.js
@@ -4,6 +4,8 @@ import MediaControlComponentPlugin from '../../base/media_control_component/medi
 export default class FullscreenButtonPlugin extends MediaControlComponentPlugin {
   get name() { return 'fullscreen_button' }
 
+  get layer() { return 1 }
+
   bindEvents() {
     const coreEventListenerData = [{ object: this.core, event: Events.CORE_ACTIVE_CONTAINER_CHANGED, callback: this.onContainerChanged }]
     coreEventListenerData.forEach(item => this.stopListening(item.object, item.event, item.callback))
