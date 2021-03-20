4acb94bd4a602dfd5cc198d791c278be3b84b312
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-20 13:55:05 -0300

    refactor(volume): define plugin render on second position of the configured section

--- src/components/volume/volume.js
+++ src/components/volume/volume.js
@@ -8,6 +8,8 @@ export default class VolumePlugin extends MediaControlComponentPlugin {
 
   get section() { return 1 }
 
+  get position() { return 2 }
+
   bindEvents() {
     const coreEventListenerData = [{ object: this.core, event: Events.CORE_ACTIVE_CONTAINER_CHANGED, callback: this.onContainerChanged }]
     coreEventListenerData.forEach(item => this.stopListening(item.object, item.event, item.callback))
