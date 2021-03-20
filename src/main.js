d1ed937687b4b528226728becbfc35128ef93369
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-20 02:40:28 -0300

    feat(main): export TimeIndicatorPlugin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import MediaControlComponentPlugin from './base/media_control_component/media_co
 import PlayPauseButtonPlugin from './components/play_pause_button/play_pause_button'
 import FullscreenButtonPlugin from './components/fullscreen_button/fullscreen_button'
 import SeekBarPlugin from './components/seek_bar/seek_bar'
+import TimeIndicatorPlugin from './components/time_indicator/time_indicator'
+
 export {
   MediaControlPlugin as MainPlugin,
   MediaControlComponentPlugin as BaseComponentPlugin,
   PlayPauseButtonPlugin,
   FullscreenButtonPlugin,
   SeekBarPlugin,
+  TimeIndicatorPlugin,
 }
