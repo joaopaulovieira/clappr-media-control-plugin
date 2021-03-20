314429a2bbd818af79b8c730e401e7a9f5186447
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-03-20 15:15:05 -0300

    feat(main): export VolumePlugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import MediaControlPlugin from './base/media_control/media_control'
 import MediaControlComponentPlugin from './base/media_control_component/media_control_component'
 import PlayPauseButtonPlugin from './components/play_pause_button/play_pause_button'
+import VolumePlugin from './components/volume/volume'
 import FullscreenButtonPlugin from './components/fullscreen_button/fullscreen_button'
 import SeekBarPlugin from './components/seek_bar/seek_bar'
 import TimeIndicatorPlugin from './components/time_indicator/time_indicator'
@@ -9,6 +10,7 @@ export {
   MediaControlPlugin as MainPlugin,
   MediaControlComponentPlugin as BaseComponentPlugin,
   PlayPauseButtonPlugin,
+  VolumePlugin,
   FullscreenButtonPlugin,
   SeekBarPlugin,
   TimeIndicatorPlugin,
